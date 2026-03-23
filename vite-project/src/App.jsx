af99c3a5fa31272146f14ccf7b0e1f78ca51d830
Derek Chiang <dereksyr@DC-MacBook-Pro.lan>
2026-03-23 15:56:44 -0400

    fix path structure

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -36,118 +36,49 @@ export default function App() {
 
       <form method="POST" action={API_URL} target="_blank">
         <label>Date RCVD</label>
-        <input
-          type="date"
-          name="dateRcvd"
-          value={form.dateRcvd}
-          onChange={handleChange}
-        />
+        <input type="date" name="dateRcvd" value={form.dateRcvd} onChange={handleChange} />
 
         <label>Last Name</label>
-        <input
-          name="lastName"
-          value={form.lastName}
-          onChange={handleChange}
-        />
+        <input name="lastName" value={form.lastName} onChange={handleChange} />
 
         <label>First Name</label>
-        <input
-          name="firstName"
-          value={form.firstName}
-          onChange={handleChange}
-        />
+        <input name="firstName" value={form.firstName} onChange={handleChange} />
 
         <label>Address</label>
-        <input
-          name="address"
-          value={form.address}
-          onChange={handleChange}
-        />
+        <input name="address" value={form.address} onChange={handleChange} />
 
         <label>Phone</label>
-        <input
-          name="phone"
-          value={form.phone}
-          onChange={handleChange}
-        />
+        <input name="phone" value={form.phone} onChange={handleChange} />
 
         <label>1st Visit</label>
-        <input
-          type="date"
-          name="firstVisit"
-          value={form.firstVisit}
-          onChange={handleChange}
-        />
+        <input type="date" name="firstVisit" value={form.firstVisit} onChange={handleChange} />
 
         <label>Feasibility Report</label>
-        <input
-          type="date"
-          name="feasibilityReport"
-          value={form.feasibilityReport}
-          onChange={handleChange}
-        />
+        <input type="date" name="feasibilityReport" value={form.feasibilityReport} onChange={handleChange} />
 
         <label>Contract Date</label>
-        <input
-          type="date"
-          name="contractDate"
-          value={form.contractDate}
-          onChange={handleChange}
-        />
+        <input type="date" name="contractDate" value={form.contractDate} onChange={handleChange} />
 
         <label>Contractor</label>
-        <input
-          name="contractor"
-          value={form.contractor}
-          onChange={handleChange}
-        />
+        <input name="contractor" value={form.contractor} onChange={handleChange} />
 
         <label>Est Start</label>
-        <input
-          type="date"
-          name="estStart"
-          value={form.estStart}
-          onChange={handleChange}
-        />
+        <input type="date" name="estStart" value={form.estStart} onChange={handleChange} />
 
         <label>1st Interim</label>
-        <input
-          type="date"
-          name="firstInterim"
-          value={form.firstInterim}
-          onChange={handleChange}
-        />
+        <input type="date" name="firstInterim" value={form.firstInterim} onChange={handleChange} />
 
         <label>2nd Interim</label>
-        <input
-          type="date"
-          name="secondInterim"
-          value={form.secondInterim}
-          onChange={handleChange}
-        />
+        <input type="date" name="secondInterim" value={form.secondInterim} onChange={handleChange} />
 
         <label>3rd Interim</label>
-        <input
-          type="date"
-          name="thirdInterim"
-          value={form.thirdInterim}
-          onChange={handleChange}
-        />
+        <input type="date" name="thirdInterim" value={form.thirdInterim} onChange={handleChange} />
 
         <label>Final</label>
-        <input
-          type="date"
-          name="finalDate"
-          value={form.finalDate}
-          onChange={handleChange}
-        />
+        <input type="date" name="finalDate" value={form.finalDate} onChange={handleChange} />
 
         <label>Status Remark</label>
-        <input
-          name="statusRemark"
-          value={form.statusRemark}
-          onChange={handleChange}
-        />
+        <input name="statusRemark" value={form.statusRemark} onChange={handleChange} />
 
         <br />
         <br />
