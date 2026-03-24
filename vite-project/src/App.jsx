62add74c6efc9d6f86f1a23a4c729427b54dd278
Derek Chiang <dereksyr@DC-MacBook-Pro.lan>
2026-03-23 21:27:25 -0400

    final native form submit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -30,60 +30,136 @@ export default function App() {
   }
 
   return (
-    <div style={{ maxWidth: 700, margin: "40px auto", padding: 20 }}>
+    <div style={{ maxWidth: 900, margin: "40px auto", padding: 20 }}>
       <h1>Client Project Tracker</h1>
       <h2>Add Client</h2>
 
       <form method="POST" action={API_URL} target="_blank">
-        <label>Date RCVD</label>
-        <input type="date" name="dateRcvd" value={form.dateRcvd} onChange={handleChange} />
+        <div style={{ display: "grid", gridTemplateColumns: "180px 1fr 180px 1fr", gap: 12, alignItems: "center" }}>
+          <label>Date RCVD</label>
+          <input
+            type="date"
+            name="dateRcvd"
+            value={form.dateRcvd}
+            onChange={handleChange}
+          />
 
-        <label>Last Name</label>
-        <input name="lastName" value={form.lastName} onChange={handleChange} />
+          <label>Last Name</label>
+          <input
+            type="text"
+            name="lastName"
+            value={form.lastName}
+            onChange={handleChange}
+          />
 
-        <label>First Name</label>
-        <input name="firstName" value={form.firstName} onChange={handleChange} />
+          <label>First Name</label>
+          <input
+            type="text"
+            name="firstName"
+            value={form.firstName}
+            onChange={handleChange}
+          />
 
-        <label>Address</label>
-        <input name="address" value={form.address} onChange={handleChange} />
+          <label>Address</label>
+          <input
+            type="text"
+            name="address"
+            value={form.address}
+            onChange={handleChange}
+          />
 
-        <label>Phone</label>
-        <input name="phone" value={form.phone} onChange={handleChange} />
+          <label>Phone</label>
+          <input
+            type="text"
+            name="phone"
+            value={form.phone}
+            onChange={handleChange}
+          />
 
-        <label>1st Visit</label>
-        <input type="date" name="firstVisit" value={form.firstVisit} onChange={handleChange} />
+          <label>1st Visit</label>
+          <input
+            type="date"
+            name="firstVisit"
+            value={form.firstVisit}
+            onChange={handleChange}
+          />
 
-        <label>Feasibility Report</label>
-        <input type="date" name="feasibilityReport" value={form.feasibilityReport} onChange={handleChange} />
+          <label>Feasibility Report</label>
+          <input
+            type="date"
+            name="feasibilityReport"
+            value={form.feasibilityReport}
+            onChange={handleChange}
+          />
 
-        <label>Contract Date</label>
-        <input type="date" name="contractDate" value={form.contractDate} onChange={handleChange} />
+          <label>Contract Date</label>
+          <input
+            type="date"
+            name="contractDate"
+            value={form.contractDate}
+            onChange={handleChange}
+          />
 
-        <label>Contractor</label>
-        <input name="contractor" value={form.contractor} onChange={handleChange} />
+          <label>Contractor</label>
+          <input
+            type="text"
+            name="contractor"
+            value={form.contractor}
+            onChange={handleChange}
+          />
 
-        <label>Est Start</label>
-        <input type="date" name="estStart" value={form.estStart} onChange={handleChange} />
+          <label>Est Start</label>
+          <input
+            type="date"
+            name="estStart"
+            value={form.estStart}
+            onChange={handleChange}
+          />
 
-        <label>1st Interim</label>
-        <input type="date" name="firstInterim" value={form.firstInterim} onChange={handleChange} />
+          <label>1st Interim</label>
+          <input
+            type="date"
+            name="firstInterim"
+            value={form.firstInterim}
+            onChange={handleChange}
+          />
 
-        <label>2nd Interim</label>
-        <input type="date" name="secondInterim" value={form.secondInterim} onChange={handleChange} />
+          <label>2nd Interim</label>
+          <input
+            type="date"
+            name="secondInterim"
+            value={form.secondInterim}
+            onChange={handleChange}
+          />
 
-        <label>3rd Interim</label>
-        <input type="date" name="thirdInterim" value={form.thirdInterim} onChange={handleChange} />
+          <label>3rd Interim</label>
+          <input
+            type="date"
+            name="thirdInterim"
+            value={form.thirdInterim}
+            onChange={handleChange}
+          />
 
-        <label>Final</label>
-        <input type="date" name="finalDate" value={form.finalDate} onChange={handleChange} />
+          <label>Final</label>
+          <input
+            type="date"
+            name="finalDate"
+            value={form.finalDate}
+            onChange={handleChange}
+          />
 
-        <label>Status Remark</label>
-        <input name="statusRemark" value={form.statusRemark} onChange={handleChange} />
+          <label>Status Remark</label>
+          <input
+            type="text"
+            name="statusRemark"
+            value={form.statusRemark}
+            onChange={handleChange}
+          />
+        </div>
 
-        <br />
-        <br />
-
-        <button type="submit">Save Client</button>
+        <div style={{ marginTop: 24 }}>
+          <button type="submit">Save Client</button>
+        </div>
       </form>
     </div>
   );
